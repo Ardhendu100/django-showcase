dbd1957f3b0ced126835e0f5ff05a3eb9d395259
ardhendu.sahoo <ardhendu.sahoo@alumnetworks.com>
2024-10-23 20:00:01 +0530

    logout code added

--- showcase/main/urls.py
+++ showcase/main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('sign-up', views.signup, name="signup"),
     path('sign-in', views.signin, name="signin"),
+    path('sign-out', views.signout, name="signout"),
 ]
