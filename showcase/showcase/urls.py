7d6d5a55561ff6c95fce142fdcbfc9119e9d00e0
ardhendu.sahoo <ardhendu.sahoo@alumnetworks.com>
2024-11-07 01:44:58 +0530

    social login added

--- showcase/showcase/urls.py
+++ showcase/showcase/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', include('main.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
